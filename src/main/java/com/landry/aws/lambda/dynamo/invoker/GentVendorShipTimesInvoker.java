e8417645b212bd1ac71fef3104c9ed32ef7a510b
Leon Landry <landryseleven@gmail.com>
2017-06-01 12:08:23 -0400

    Clean up

--- src/main/java/com/landry/aws/lambda/dynamo/invoker/GentVendorShipTimesInvoker.java
+++ src/main/java/com/landry/aws/lambda/dynamo/invoker/GentVendorShipTimesInvoker.java
@@ -3,10 +3,11 @@ package com.landry.aws.lambda.dynamo.invoker;
 import java.util.Set;
 
 import com.amazonaws.services.lambda.invoke.LambdaFunction;
+import com.landry.aws.lambda.common.util.LambdaFunctions;
 import com.landry.aws.lambda.dynamo.domain.VendorShipTime;
 
 public interface GentVendorShipTimesInvoker
 {
-	@LambdaFunction(functionName = "getVendorShipTimes")
+	@LambdaFunction(functionName = LambdaFunctions.GET_VENDOR_SHIP_TIMES)
 	Set<VendorShipTime> getVendorShipTimes( String input );
 }
