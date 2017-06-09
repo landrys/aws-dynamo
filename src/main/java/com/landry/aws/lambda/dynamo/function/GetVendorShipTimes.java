3cd0587da37268f44c28c57e147da956c5b74ad8
Leon Landry <landryseleven@gmail.com>
2017-06-09 10:19:27 -0400

    Sorted Vendor Ship Times alphabetically.

--- src/main/java/com/landry/aws/lambda/dynamo/function/GetVendorShipTimes.java
+++ src/main/java/com/landry/aws/lambda/dynamo/function/GetVendorShipTimes.java
@@ -1,9 +1,13 @@
 package com.landry.aws.lambda.dynamo.function;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.landry.aws.lambda.common.util.LambdaFunctions;
 import com.landry.aws.lambda.dynamo.dao.DynamoVendorShipTimeDAO;
 import com.landry.aws.lambda.dynamo.domain.VendorShipTime;
 
@@ -18,12 +22,23 @@ public class GetVendorShipTimes implements RequestHandler<String, List<VendorShi
 	@Override
 	public List<VendorShipTime> handleRequest( String input, Context context )
 	{
+		System.out.println(LambdaFunctions.GET_VENDOR_SHIP_TIMES);
 		if ( input==null )
 			return null;
 		List<VendorShipTime> data = vstDao.findAll();
 		for ( VendorShipTime bean : data )
 			if ( bean.getWarehouse() == null )
 				bean.setWarehouse(":");
-		return data;
+		
+		List<VendorShipTime> newList = new ArrayList<VendorShipTime>(data);
+
+
+		Collections.sort(newList, new Comparator<VendorShipTime>(){
+			  public int compare(VendorShipTime p1, VendorShipTime p2){
+			    return p1.getName().compareTo(p2.getName());
+			  }
+			});
+
+		return newList;
 	}
 }
