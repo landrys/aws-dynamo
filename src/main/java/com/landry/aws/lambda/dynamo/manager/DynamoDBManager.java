f1347cf986e854f30d2625374c83c0ea966f75c9
Leon Landry <landryseleven@gmail.com>
2017-07-10 09:49:11 -0400

    Added LCAuthClient table for LC Proxy. Some local testing with DynamoDB on development machine.

--- src/main/java/com/landry/aws/lambda/dynamo/manager/DynamoDBManager.java
+++ src/main/java/com/landry/aws/lambda/dynamo/manager/DynamoDBManager.java
@@ -1,8 +1,14 @@
 package com.landry.aws.lambda.dynamo.manager;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder.EndpointConfiguration;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.landry.aws.lambda.common.util.MyConstants;
 
 public class DynamoDBManager
 {
@@ -13,8 +19,27 @@ public class DynamoDBManager
 
 	private DynamoDBManager()
 	{
-		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
-		mapper = new DynamoDBMapper(client);
+		/*
+		 * Test configuration
+		 */
+		if (MyConstants.TESTING)
+		{
+			EndpointConfiguration endpointConfiguration = new EndpointConfiguration("http://localhost:8000",
+					"us-east-1");
+			AWSCredentials awsCreds = new BasicAWSCredentials("AKIAITXA4Q4J3CJTYP6Q", "1234");
+			AWSCredentialsProvider credentialsProvider = new AWSStaticCredentialsProvider(awsCreds);
+			AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+					.withEndpointConfiguration(endpointConfiguration).withCredentials(credentialsProvider).build();
+			mapper = new DynamoDBMapper(client);
+		}
+		else
+		{
+			/*
+			 * Production configuration
+			 */
+			AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient();
+			mapper = new DynamoDBMapper(client);
+		}
 	}
 
 	public static DynamoDBManager instance()
